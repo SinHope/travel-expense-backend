8ca31da3589285aff89291f218388734ef8487c6
Nasmer Fontanilla <nasmerfontanilla@gmail.com>
2025-06-08 21:04:59 +0800

    latest update

--- server.js
+++ server.js
@@ -21,5 +21,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/trips",tripRoutes); // this connects /api/trips to your router
 
+app.get('/ping', (req, res) => res.send('pong'));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
